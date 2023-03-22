34a7718793fe91cfdc08c32e46391968fd4315f4
nguyen.d1612 <nguyen.d1612@gmail.com>
2023-03-21 21:18:58 -0500

    added youtube video embed

--- assets/script.js
+++ assets/script.js
@@ -40,10 +40,38 @@ function handleFormSubmit(event) {
       console.log(data);
     });
 
-  console.log(movie);
+  //console.log(movie); redundant
+  
 }
 
 //
 
 
   titleEl.on('submit', handleFormSubmit);
+
+    $(document).ready(function(){
+        let apiKey = "AIzaSyB5faftGXgqJpv-C13VoHtm_UDWcPkovvs"
+     
+        $("form").submit((e) => {
+            e.preventDefault()
+            let search = $("#search").val()
+            videoSearch(apiKey,search,1)
+        })
+    })
+     
+    function videoSearch(apiKey,search,maxResults){
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + apiKey + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,(data) => {
+            console.log(data)
+            
+            let video = ''
+     
+            data.items.forEach(item => {
+                video = `
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                `
+     
+                $("#videos").append(video)
+            });
+        })
+     
+    }
